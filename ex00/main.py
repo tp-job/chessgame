9a9d34a25cb27a3d7ba5ffb66364bcb9355735f4
tp_job <phutiwanich@gmail.com>
2024-11-21 17:28:46 +0700

    Add files via upload

---
+++ ex00/main.py
@@ -0,0 +1,14 @@
+from checkmate import checkmate
+
+# cheak king on board
+def main():
+    board = """\
+R...
+.K..
+.B..
+.P..\
+"""
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
